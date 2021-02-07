62ac2259d6a3d343cca17fe0c1e8c277ec7d1b3f
Pragyesh <pragyeshtiwari29@gmail.com>
2021-02-07 11:50:04 +0530

    react animation components

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -4,32 +4,42 @@ import {Control, LocalForm, Errors} from 'react-redux-form';
 import {Link} from 'react-router-dom';
 import {Loading} from './LoadingComponent';
 import {baseUrl} from '../shared/baseUrl';
+import {FadeTransform, Fade, Stagger} from 'react-animation-components';
 
     function RenderDish({dish}){
         return (
-            <Card>
-                <CardImg top width="100%" src={baseUrl + dish.image} alt={dish.name}/>
-                <CardBody>
-                    <CardTitle>{dish.name}</CardTitle>
-                    <CardText>{dish.description}</CardText>
-                </CardBody>
-            </Card>
+            <FadeTransform in
+                transformProps = {{
+                    exitTransform: 'scale(0.5) translateY(-50%)'
+                }} >
+                <Card>
+                    <CardImg top width="100%" src={baseUrl + dish.image} alt={dish.name}/>
+                    <CardBody>
+                        <CardTitle>{dish.name}</CardTitle>
+                        <CardText>{dish.description}</CardText>
+                    </CardBody>
+                </Card>
+            </FadeTransform>
         );
     }
 
     function RenderComments({comments, postComment, dishId}){
         const comment = comments.map(comment => {
             return (
-                <li>
-                    {comment.comment}<br/>
-                    --{comment.author}, {new Intl.DateTimeFormat('en-US', {year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}<br/><br/>
-                </li>
+                <Fade in>
+                    <li>
+                        {comment.comment}<br/>
+                        --{comment.author}, {new Intl.DateTimeFormat('en-US', {year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}<br/><br/>
+                    </li>
+                </Fade>
             );
         });
         return (
             <>
                 <ul className="list-unstyled">
-                    {comment}
+                    <Stagger in>
+                        {comment}
+                    </Stagger>
                 </ul>
                 <CommentForm dishId={dishId} postComment={postComment}/>
             </>
